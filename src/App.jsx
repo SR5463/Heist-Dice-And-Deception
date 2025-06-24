5ef9b3a1ee1107db113587da41625cce638b7978
Sul <Sulaimaan.rawat@gmail.com>
2025-06-24 10:12:42 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import RoleAssignment from './components/RoleAssignment';
 import DiceRoller from './components/DiceRoller';
 import ProbabilityCalculator from './components/ProbabilityCalculator';
+import Tutorial from './components/Tutorial';
 import './App.css';
 
 export default function App() {
@@ -11,9 +12,11 @@ export default function App() {
     <div className="app-container">
       <h1>Heist: Dice & Deception</h1>
       <RoleAssignment players={players} setPlayers={setPlayers} />
+      {/* Tutorial Section */}
+      <Tutorial filePath="/tutorial.txt" />
+
       {players.length > 0 && (
         <div className="game-interact">
-          {/* Pass players down so DiceRoller can cycle through names */}
           <DiceRoller players={players} />
           <ProbabilityCalculator />
         </div>
